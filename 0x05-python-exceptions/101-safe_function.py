92ac92e5ab813e105d16f2290bef00e75a30ef01
BKiptoo · he <bettsonk@gmail.com>
2023-10-23 04:32:36 -0700

    Updated 101-safe_function.py with new content

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct,*args):
     from sys import stder
     try:
-        result=fct(*args)
+        result = fct(*args)
         return result
     except Exception as err:
         print ('Exception:{}'.format(err),file=stder
